5e5a9b6c9558db2f3dfaff6c76f9b7a3cc11d5bf
Evgen <kurakin.evgeny@gmail.com>
2022-01-17 15:28:23 +0300

    создали приложение на паттерне redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
+import * as actions from './store/actionTypes'
+import { createStore } from './store/createStore'
+import { taskReducer } from './store/taskReducer'
+
+const initialState = [
+  { id: 1, title: 'Task 1', completed: false },
+  { id: 2, title: 'Task 2', completed: false }
+]
+const store = createStore(taskReducer, initialState)
 
 const App = () => {
-  return <h1> App </h1>
+  const [state, setState] = useState(store.getState())
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState())
+    })
+  }, [])
+
+  const completeTask = (taskId) => {
+    store.dispatch({
+      type: actions.taskUpdated,
+      payload: { id: taskId, completed: true }
+    })
+  }
+
+  const changeTitle = (taskId) => {
+    store.dispatch({
+      type: actions.taskUpdated,
+      payload: { id: taskId, title: `New title for ${taskId}` }
+    })
+  }
+
+  return (
+    <>
+      <h1> App </h1>
+
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{`Complited: ${el.completed}`}</p>
+            <button onClick={() => completeTask(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change titlt</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 ReactDOM.render(

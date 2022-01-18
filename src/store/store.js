1dc7cc7846479fea1b55a1d2c47f2dd028dd17f4
Evgen <kurakin.evgeny@gmail.com>
2022-01-18 17:17:23 +0300

    Подключил Redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,11 @@ import { createStore } from 'redux'
 import taskReducer from './task'
 
 function configureStore() {
-  return createStore(taskReducer)
+  return createStore(
+    taskReducer,
+    // Если есть redux devtools расширение chrome
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 }
 
 export default configureStore
